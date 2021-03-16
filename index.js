616e258229770da1b0a206ca5b3a2bacbd702e7d
AdamDIOM <adam.drummond9@gmail.com>
2021-03-16 20:30:43 +0000

    restructure

--- index.js
+++ index.js
@@ -6,32 +6,48 @@ function About(){
     section.innerHTML = "<h1 id=\"sectionHeading\">About</h1>"
 }
 
-function TeamMembers(){
-    section.innerHTML = `
-        <h1 id="sectionHeading">Team Members</h1>
-        ${AddImageBox("Assets/Avatar.ico", "Adam Drummond", "MELONsquad Leader. Bringer of melons to MLH. Also loves a game of Chrome Dino!")}
-        ${AddImageBox("Assets/blahaj.png", "BLAHAJ", "King of <a href=\"https://blahajgang.lol\">BLAHAJgang</a>, honorary fellow of MELONsquad. We love BLAHAJ!")}
-        ${AddImageBox("Assets/ryan.png", "Ryan Swift", "#1 fan of MELONsquad, wishes it was a separate entity to <a href=\"https://blahajgang.lol\">BLAHAJgang</a> (never gonna happen!!)")}
-        ${AddImageBox("Assets/jack.png", "Jacklyn Biggin", "Leader of <a href=\"https://blahajgang.lol\">BLAHAJgang</a>, unable to consume melon (allergies - RIP). Honorary fellow of MELONsquad.")}
-        `
+async function TeamMembers(){
+    section.innerHTML = "<h1 id=\"sectionHeading\">Team</h1>"
+    team = await AddImageBoxes("Team", "team");
+    console.log(team);
+    section.innerHTML += team;
 }
 
 function History(){
     section.innerHTML = "<h1 id=\"sectionHeading\">History</h1>"
 }
 
-function Art(){
+async function Art(){
     section.innerHTML = "<h1 id=\"sectionHeading\">Art</h1>"
+    art = await AddImageBoxes("Art", "art");
+    console.log(art);
+    section.innerHTML += art;
+}
+
+async function AddImageBoxes(imageDirectory, jsonFile){
+    const response = await fetch("JSON/" + jsonFile + '.json')
+    .then(response => response.json());
+    json = response
+    //console.log(json);
+    team = "";
+    for(user in json){
+        //console.log(user);
+        //console.log(team);
+        team += AddImageBox(imageDirectory + "/" + json[user].image, json[user].name, json[user].caption);
+    }
+    //console.log(team);
+
+    return team;
 }
 
 function AddImageBox(source, title, caption){
-    string = `
+    box = `
         <div id="wideBox">
-            <img src="${source}" id="boxImg">
+            <img src="Assets/${source}" id="boxImg">
             <p id="title">${title}</p>
             <p id="caption">${caption}</p>
         </div>
     `
 
-    return string;
+    return box;
 }
