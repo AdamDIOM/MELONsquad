ae8daf1ef3d460457e3f5b7443599bb3473b8dea
AdamDIOM <adam.drummond9@gmail.com>
2021-03-16 16:54:21 +0000

    Added basic MELONsquad features

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function update(){
-    console.log("updating...");
-    for(i = 0; i < 9; i++){
-        document.getElementById(i).innerHTML = document.getElementById(`input${i}`).value;
-    }
+function MELONsquad(){
+
+}
+
+function About(){
+
+}
+
+function TeamMembers(){
+
+}
+
+function History(){
+
+}
+
+function Art(){
+    
 }
