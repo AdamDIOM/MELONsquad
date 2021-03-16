2e7d16b09f9396270158532dbd4ebcfb5bba6dbe
AdamDIOM <adam.drummond9@gmail.com>
2021-03-16 17:03:56 +0000

    added base testing JS

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 function MELONsquad(){
-
+    section.innerHTML = "<h1 id=\"sectionHeading\">MELONsquad</h1>"
 }
 
 function About(){
-
+    section.innerHTML = "<h1 id=\"sectionHeading\">About</h1>"
 }
 
 function TeamMembers(){
-
+    section.innerHTML = "<h1 id=\"sectionHeading\">Team Members</h1>";
+    section.innerHTML += "<img src=\"Assets/Avatar.ico\">";
+    section.innerHTML += "Adam Drummond";
 }
 
 function History(){
-
+    section.innerHTML = "<h1 id=\"sectionHeading\">History</h1>"
 }
 
 function Art(){
-    
+    section.innerHTML = "<h1 id=\"sectionHeading\">Art</h1>"
 }
